5576b5eae7c80a6f32078deeec96e33f986297b1
chinmaygireesh <chinmaygireeshs@gmail.com>
2021-07-09 13:40:36 +0530

    REFACTOR:CHANGES THE CODE STRUCTUE
    This commit changes the parantheses into a new line

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace leader_in_an_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = new int[]  { 3,3,3};
+            int i,j;
+            bool flag;
+            for(i=0;i<arr.Length;i++)
+            {
+                
+                flag = true;//assuming as a leader
+                for(j=i+1;j<arr.Length;j++)
+                {
+                    if(arr[j]>arr[i])
+                    {
+                        flag = false; //not a leader
+                        break;
+                    }
+                    }
+                if(flag)//still if it is leader
+                {
+                    
+                    Console.Write($"{arr[i]} ");
+                }    
+                
+            }
+        }
+    }
+}
